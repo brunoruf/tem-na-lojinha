e8b3c3303d0dbb1af6149e689c29a4dc7cac2ff3
brunoruf <brunoruf@hotmail.com>
2025-04-23 12:48:57 -0300

    Adding style

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,7 @@ import './style.css'
 export const ProductCard = ({name, price, barcode, quantity}) => {
   return (
     <div className="product-card">
-        <h3>{name}</h3>
+        <h3 className='product-title'>{name}</h3>
         <div className='product-info'>
           <p>Quantidade na loja: <strong>{quantity}</strong></p>
           <p>Preço: <strong>R$ {price.toFixed(2)}</strong></p>
